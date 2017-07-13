7c9e934bfbb27075a8ece4fee757d1d804c7e6ce
Chris Ward <cward@redhat.com>
2017-07-13 15:02:23 +0200

    i18n support

--- deploy.py
+++ deploy.py
@@ -2,16 +2,22 @@
 # -*- coding: utf-8 -*-
 
 
-# from functools import partial
+import functools
 import logging
 import os
 import shutil
+import subprocess
 
 from babel.support import Translations
 import click
 import confuse
 import ipdb  # NOQA
 import jinja2
+
+# LOGGING
+# Setup console logging
+log = logging.getLogger('devconf')
+logging.basicConfig(format='%(message)s')
 
 
 def dumps(content, path):
@@ -26,11 +32,19 @@ def mkdirs(path):
         os.makedirs(path)
 
 
-def ext_url(path):
-    path_build = os.environ['PATH_BUILD']
+def ext_url(path, static=False):
+    path_build = os.path.abspath(os.environ['PATH_BUILD'])
+    locale = os.environ['LOCALE']
+    lang = locale.split('_')[0]
     path = path.strip()
     if path[0] == '/':
         path = path.lstrip('/')
+        if static:
+            path = os.path.join(path_build, path)
+        else:
+            path = os.path.join(path_build, lang, path)
+        path = 'file://{}'.format(path)
+    else:
         path = os.path.join(path_build, path)
         path = 'file://{}'.format(path)
     return path
@@ -54,51 +68,17 @@ _loader = jinja2.FileSystemLoader(PATH_TEMPLATES)
 
 # FIXME: move this to 'init' command or something...
 locale_dir = './i18n'
-# prepare the build
-if not os.path.exists(locale_dir):
-    mkdirs(locale_dir)
 
-msgdomain = 'html'
-default_locale = 'en_US'
-list_of_desired_locales = ['en', 'cs', 'sk']
 extensions = ['jinja2.ext.i18n', 'jinja2.ext.with_']
 
-'''
-Create the folder structure (no whitespace after the commas!!!)
-> mkdir -pv ./il8n/{en_US,cs_CZ,sk_SK}/LC_MESSAGES/
-> pybabel -v extract -F babel.cfg -o ./il8n/messages.pot ./
-
-Init/Update
-3.1 Init
-> pybabel init -l en_US -d ./il8n -i ./il8n/messages.pot
-> pybabel init -l cs_CZ -d ./il8n -i ./il8n/messages.pot
-> pybabel init -l sk_SK -d ./il8n -i ./il8n/messages.pot
-
-3.2 Update
-> pybabel update -l en_US -d ./il8n -i ./il8n/messages.pot
-> pybabel update -l cs_CZ -d ./il8n -i ./il8n/messages.pot
-> pybabel update -l sk_SK -d ./il8n -i ./il8n/messages.pot
-
-Compile
-> pybabel compile -f -d ./il8n
-'''
-
-translations = Translations.load(locale_dir, list_of_desired_locales)
 jinja2_env = jinja2.Environment(loader=_loader,
                                 autoescape=True,
                                 extensions=extensions)
-jinja2_env.install_gettext_translations(translations)
-
-PATH_STATIC = './static'
-
-# LOGGING
-# Setup console logging
-log = logging.getLogger('devconf')
-logging.basicConfig(format='%(message)s')
-
-
 # add the ext to the jinja environment
 jinja2_env.filters['url'] = ext_url
+jinja2_env.filters['static'] = functools.partial(ext_url, static=True)
+
+PATH_STATIC = './static'
 
 
 @click.group()
@@ -161,24 +141,79 @@ def build(branch, clean):
             # FIXME: only if DEBUG on, otherwise don't include private keys
             '__site': site,
             '__branch': branch,
-            '__template': path,
+            'path': path,
+            'flags_path': {
+                'en': '/images/lang_flag_en.svg',
+                'cs': '/images/lang_flag_cs.svg',
+                'sk': '/images/lang_flag_sk.svg',
+            },
         }
 
-        # Render the template
-        template = jinja2_env.get_template(path)
-        content = template.render(**params)
+        locales = ['en_US', 'cs_CZ', 'sk_SK']
+        for locale in locales:
+            params['locale'] = locale
+            os.environ['LOCALE'] = locale
+            lang = locale.split('_')[0]
+            params['lang'] = lang
+            translations = Translations.load(locale_dir, [locale])
+            jinja2_env.install_gettext_translations(translations, newstyle=True)
 
-        # Save the rendered page to disk
-        dest_file = os.path.join(PATH_BUILD, path)
-        # create the directory struct where the file will live
-        mkdirs(os.path.dirname(dest_file))
-        # dump the rendered file
-        dumps(content, dest_file)
+            # Render the template
+            template = jinja2_env.get_template(path)
+            content = template.render(**params)
 
-        assert os.path.exists(dest_file)
+            # FIXME: WHY NOT USING BRANCH?
+
+            # Save the rendered page to disk
+            dest_file = os.path.join(PATH_BUILD, lang, path)
+            # create the directory struct where the file will live
+            mkdirs(os.path.dirname(dest_file))
+            # dump the rendered file
+            dumps(content, dest_file)
+
+            assert os.path.exists(dest_file)
 
 
+@cli.command('i18n')
+@click.argument('action', nargs=-1)
+def i18n(action):
+    '''
+    Create the folder structure (no whitespace after the commas!!!)
+    > mkdir -pv ./i18n/{en_US,cs_CZ,sk_SK}/LC_MESSAGES/
+    > pybabel -v extract -F babel.cfg -o ./i18n/messages.pot ./
 
+    Init/Update
+    3.1 Init
+    > pybabel init -l en_US -d ./i18n -i ./i18n/messages.pot
+    > pybabel init -l cs_CZ -d ./i18n -i ./i18n/messages.pot
+    > pybabel init -l sk_SK -d ./i18n -i ./i18n/messages.pot
+
+    3.2 Update
+    > pybabel update -l en_US -d ./i18n -i ./i18n/messages.pot
+    > pybabel update -l cs_CZ -d ./i18n -i ./i18n/messages.pot
+    > pybabel update -l sk_SK -d ./i18n -i ./i18n/messages.pot
+
+    Compile
+    > pybabel compile -f -d ./i18n
+
+    What if the strings change? Create a new messages.pot and merge the changes:
+    > pybabel update -d ./i18n -i ./i18n/messages.pot
+    '''
+
+    languages = ['en_US', 'cs_CZ', 'sk_SK']
+    if 'init' in action:
+        cmd = 'pybabel init -l {} -d ./i18n -i ./i18n/messages.pot'
+        for lang in languages:
+            subprocess.run(cmd.format(lang), shell=True)
+
+    if 'update' in action:
+        cmd = 'pybabel update -l {} -d ./i18n -i ./i18n/messages.pot'
+        for lang in languages:
+            subprocess.run(cmd.format(lang), shell=True)
+
+    if 'compile' in action:
+        cmd = 'pybabel compile -f -d ./i18n'
+        subprocess.run(cmd, shell=True)
 
 
 if __name__ == '__main__':
