2101e865778138abaf8b8d2a3401bd3267a32eff
Josef Řídký <jridky@redhat.com>
2020-09-23 16:49:24 +0200

    Update CZ page and FAQ

--- source/cz/index.blade.php
+++ source/cz/index.blade.php
@@ -1,8 +1,8 @@
 @extends('_layouts.page')
 
 @section('social')
-<meta property="og:title" content="DevConf.cz | Brno, Czechia">
-<meta property="og:site_name" content="DevConf.cz">
+<meta property="og:title" content="DevConf.CZ | Brno, Czechia">
+<meta property="og:site_name" content="DevConf.CZ">
 <meta property="og:description" content="DevConf are free and open, annual, open-source community conferences for community contibutors, sponsored by Red Hat.">
 <meta property="og:type" content="website">
 <meta property="og:url" content="https://devconf.info">
@@ -30,7 +30,7 @@
 		</button>
 		<div class="collapse navbar-collapse" id="navbarMobile">
 			<ul class="navbar-nav ml-auto pr-5">
-<!--				<li class="nav-item active px-3">
+				<li class="nav-item active px-3">
 					<a href="#about" class="nav-link local">About & Topics</a>
 				</li>
 				<li class="nav-item active px-3">
@@ -39,12 +39,12 @@
 				<li class="nav-item active px-3">
 					<a href="#schedule" class="nav-link local">Schedule</a>
 				</li>
-				<li class="nav-item active px-3">
+<!--				<li class="nav-item active px-3">
 					<a href="#travel" class="nav-link local">How to get here?</a>
-				</li>
+				</li> -->
 				<li class="nav-item active px-3">
 					<a href="/cz/faq" class="nav-link local">FAQ</a>
-				</li> -->
+				</li> 
 				<li class="nav-item active px-3">
 					<a href="/coc" class="nav-link local" target="_blank">Policies</a>
 				</li>
@@ -83,18 +83,17 @@
 		</div>
 	</div>
 </header>
-<!--
 <section id="about" class="mx-auto col-lg-11">
 	<div class="row mt-4 py-5">
 		<div class="col-lg-4 mx-5 mb-5 text-center">
-			<img src="/assets/images/devconf-d-brno.png" class="mw-100" alt="Brno DevConf image asset">
+			<img src="/assets/images/devconf-d-virtual.png" class="mw-100" alt="Virtual DevConf image asset">
 		</div>
 		<div class="col-lg-6 px-5">
 			<h1 class="mont-700 underline pb-4">About DevConf</h1>
 			<div class="os-400">
-                <p>DevConf.CZ 2020 is the 12th annual, free, Red Hat sponsored community conference for developers, admins, DevOps engineers, testers, documentation writers and other contributors to open source technologies.
-                   The conference includes topics on Linux, Middleware, Virtualization, Storage, Cloud and mobile.
-                   At DevConf.CZ, FLOSS communities sync, share, and hack on upstream projects together in the beautiful city of Brno, Czech Republic.</p>
+                <p>DevConf.CZ 2021 is the 13th annual, free, Red Hat sponsored virtual community conference for developers, admins, DevOps engineers, testers, documentation writers and other contributors to open source technologies.
+                   The conference includes topics on Linux, Middleware, Virtualization, Storage, Cloud and mobile. At DevConf.CZ, FLOSS communities sync, share, and hack on upstream projects together.</p>
+                <p>While we are disappointed that we won’t be able to see you all in person, we're shifting to an awesome virtual event in 2021!</p>
 
 				<p>There is no admission or ticket charge for DevConf.CZ events. However, you are required to complete a free registration. Watch this site for updates about registration.</p>
 
@@ -114,18 +113,17 @@
 
 				$topics = array(
 						array("name"=>"Agile, DevOps & CI/CD","description"=>"The Agile, DevOps & CI/CD track is intended to address the practices, methods and tooling that technical audiences need to deliver increased business value and responsiveness through rapid, high-quality delivery. Topics include engineering best practices, defining and building Agile & DevOps cultures, Open Source tools to make Developers and Operations successful, continuous integration/delivery/deployment of enterprise software, and many more."),
-						array("name"=>"Blockchain","description"=>"Blockchain related technology or use-cases leveraging the Hyperledger framework. Do not submit ICOs or pitch talks to the altcoin where you invested a lot of money."),
 						array("name"=>"Cloud and Containers","description"=>"Cloud, containers, clusters. Buildah, CRI-O, Docker, Kubernetes, Skopeo. Hybrid cloud and other buzzwords - your cloud and container topics go here. Daemons of all sizes and shapes."),
 						array("name"=>"Community","description"=>"Building open source software is more than just about coding. Learn about the many aspects of creating and maintaining healthy, robust, and diverse open source communities."),
-                                                array("name"=>"Debug / Tracing","description"=>"Developer tools for debugging and tracing your application and its enviroment.  Kernel level to userland application level."),
-                                                array("name"=>"Desktop","description"=>"Traditional and new package management tools, GUI applications, optimizations for the desktop, anything desktop-related goes here."),
-                                                array("name"=>"Developer Tools","description"=>"Developer tools for planning, creating, compiling, running, and analyzing code. Including all programming languages, frameworks and online development environments."),
-                                                array("name"=>"Documentation","description"=>"Guides, manuals, howtos, articles, and other kinds of help content. New ideas, documentation workflows, user-story focus, modularity, and automation."),
-                                                array("name"=>"Fedora","description"=>"All things Fedora!"),
-                                                array("name"=>"Frontend / UI / UX","description"=>"The Frontend/UX/UI track is intended to address user experience design, user interface design, and front end development. Topics include: processes and tools related to design and frontend development, approaches to research and collecting user feedback, and strategies towards increased transparency for design and development across teams."),
-                                                array("name"=>"Kernel","description"=>"All things Linux Kernel"),
+                        array("name"=>"Debug / Tracing","description"=>"Developer tools for debugging and tracing your application and its enviroment.  Kernel level to userland application level."),
+                        array("name"=>"Desktop","description"=>"Traditional and new package management tools, GUI applications, optimizations for the desktop, anything desktop-related goes here."),
+                        array("name"=>"Developer Tools","description"=>"Developer tools for planning, creating, compiling, running, and analyzing code. Including all programming languages, frameworks and online development environments."),
+                        array("name"=>"Documentation","description"=>"Guides, manuals, howtos, articles, and other kinds of help content. New ideas, documentation workflows, user-story focus, modularity, and automation."),
+                        array("name"=>"Fedora","description"=>"All things Fedora!"),
+                        array("name"=>"Frontend / UI / UX","description"=>"The Frontend/UX/UI track is intended to address user experience design, user interface design, and front end development. Topics include: processes and tools related to design and frontend development, approaches to research and collecting user feedback, and strategies towards increased transparency for design and development across teams."),
+                        array("name"=>"Kernel","description"=>"All things Linux Kernel"),
 						array("name"=>"Immutable OS","description"=>"Everything from the previous CoreOS and Atomic operating systems to the new Fedora CoreOS, an automatically updating, minimal, monolithic, container-focused operating system, designed for clusters while also operable standalone. Server-related topics around rpm-ostree too."),
-                                                array("name"=>"IoT (Internet of Things)","description"=>"Data crunching applications, microcontrollers, protocols and embedded software. Development and testing tools. Deployment and maintenance techniques. Security challenges and interesting use cases."),
+                        array("name"=>"IoT (Internet of Things)","description"=>"Data crunching applications, microcontrollers, protocols and embedded software. Development and testing tools. Deployment and maintenance techniques. Security challenges and interesting use cases."),
 						array("name"=>"Microservices","description"=>"Microservices architecture, design, challenges, patterns and testing. Including RHOAR based technologies (Vert.x, Spring Boot, Node.js, Thorntail, MicroProfile, Fuse) and other platforms, including development & production related topics."),
 						array("name"=>"Middleware","description"=>"Infrastructure exists to run applications, and applications are built on Middleware.  Red Hat has several runtimes and services designed to integrate with our platforms for efficient performance and rapid development."),
 						array("name"=>"ML / AI / Big Data","description"=>"Development tools for data engineering and data science, model development and deployment, interesting models to solve problems, e.g., in software engineering, system/cloud administration."),
@@ -166,23 +164,23 @@
 		<div class="h1 underline pb-4 mx-5 position-relative mont-700">Important dates</div>
 	</div>
 	<div class="row">
-		<!-- Keep in pairs ->
+		<!-- Keep in pairs -->
 		<div class="col-10 col-md-5 mr-md-0 mx-auto py-3 border-bottom">
 			<div class="row">
-				<div class="col-4 h5 mont-700">Sep 24, 2019</div>
+				<div class="col-4 h5 mont-700">Sep 24, 2020</div>
 				<div class="col-8">
 					<div class="row">
 						<div class="col mont-600">CfP Opens</div>
 					</div>
-<!--					<div class="row">
+					<div class="row">
 						<div class="col os-400"><a href="https://cfp.devconf.info">Submit your proposal &rarr;</a></div>
 					</div>
-->				</div>
+				</div>
 			</div>
 		</div>
 		<div class="col-10 col-md-5 ml-md-0 mx-auto py-3 border-bottom">
 			<div class="row">
-				<div class="col-4 h5 mont-700">Nov 1, 2019</div>
+				<div class="col-4 h5 mont-700">Nov 1, 2020</div>
 				<div class="col-8">
 					<div class="row">
 						<div class="col mont-600">CfP Closes</div>
@@ -191,23 +189,23 @@
 			</div>
                 </div>
 
-		<!-- Second pair ->
+		<!-- Second pair -->
 		<div class="col-10 col-md-5 mr-md-0 mx-auto py-3 border-bottom">
 			<div class="row">
-				<div class="col-4 h5 mont-700">Nov 19, 2019</div>
+				<div class="col-4 h5 mont-700">Nov 30, 2020</div>
 				<div class="col-8">
 					<div class="row">
-						<div class="col mont-600">Speaker Acceptance Letters Sent</div>
+						<div class="col mont-600">1st round of speaker acceptance letters</div>
 					</div>
 				</div>
 			</div>
                 </div>
 		<div class="col-10 col-md-5 ml-md-0 mx-auto py-3 border-bottom">
 			<div class="row">
-				<div class="col-4 h5 mont-700">Dec 9, 2019</div>
+				<div class="col-4 h5 mont-700">Jan 11, 2021</div>
 				<div class="col-8">
 					<div class="row">
-						<div class="col mont-600">Registration Opens and Schedule Published</div>
+						<div class="col mont-600">Registration opens</div>
 					</div>
 				</div>
 			</div>
@@ -218,20 +216,10 @@
 	</div>
 	<div class="row">
 		<div class="col-10 mx-auto">
-			<p class="os-400">Attendee registration for <strong>Red Hat Alumni II</strong> meetup is open <a href="https://redhat.avature.net/eventslisting/JobDetail?jobId=15409" target="_blank">here</a>.</p>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-10 mx-auto">
 			<p class="os-400">Join <a href="https://t.me/devconfcz" target="_blank">Telegram DevConf.CZ Attendees group chat</a>! Connect with other attendees and get instant updates from the organizers!</p>
 		</div>
 	</div>
-	<div class="row">
-		<div class="col-10 mx-auto">
-			<p class="os-400">Conference rooms are assigned based on attendee's preferences. See <a href="#schedule">schedule</a>.</p>
-		</div>
-	</div>
-	<div class="row">
+<!--	<div class="row">
 		<div class="col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-2 mx-auto">
 			This is the list of booths you can expect at DevConf.CZ this year. Get ready for some stickers and so much more!
 			<ul class="timeline timeline-centered pt-5">
@@ -285,13 +273,13 @@
 			</ul>
 		</div>
 	</div>
-
+-->
 </section>
 <section id="schedule" class="mx-auto col-lg-12 background-gray text-center mt-5 py-5">
-	<div class="h1 mont-700 underline position-relative py-4 mt-3">Schedule</div>
-	<a class="p-3 mont-700 mb-3 d-inline-block purple-href" target="_blank" href="https://devconfcz2020a.sched.com/">SEE SCHEDULE</a>
+	<div class="h1 mont-700 position-relative py-4 mt-3">Schedule</div>
+	<a class="p-3 mont-700 mb-3 d-inline-block purple-href" href="#">Will be announced in January</a>
 </section>
-<section id="travel" class="mx-auto py-5 col-lg-11">
+<!--<section id="travel" class="mx-auto py-5 col-lg-11">
 	<div class="row">
 		<div class="mont-700 h1 underline position-relative mx-5 pb-4">How to get there?</div>
 	</div>
