131e988442196abc138b831d69615bebbe8e817e
Josef Řídký <jridky@redhat.com>
2021-12-09 16:26:59 +0100

    Update CZ FAQ

--- source/index.blade.php
+++ source/index.blade.php
@@ -40,7 +40,7 @@
 				<div class="card-body text-center px-4">
 					<a class="underline" href="/cz">
 						<div class="d-block mx-auto pb-3"><img src="/assets/images/cz-flag.svg" alt="CZ flag"></div>
-						DevConf.CZ - Hybrid
+						DevConf.CZ - Virtual
 					</a>
 				</div>
 				<div class="card-body text-center px-4">
