2cfd27937cc91c04fe062c8a51ae08a37e26286a
Josef Řídký <jridky@redhat.com>
2022-01-13 13:17:13 +0100

    Add WorkAdventure FAQ

---
+++ source/cz/workadventure/info.blade.php
@@ -0,0 +1,27 @@
+@extends('_layouts.noads')
+
+@section('title')
+<title>Opportunities are open!</title>
+@endsection
+
+@section('body')
+<!-- Full Page Image Header with Vertically Centered Content -->
+<header class="h-100">
+  <div class="container h-100">
+    <h1>WorkAdventure FAQ</h1>
+<p>Hello and welcome to DevConf WorkAdventure map!</p>
+
+<p><strong>Q: How does this work?<br>A:</strong> You can move around pressing keys: W/A/S/D (or arrow keys), you can explore the map and most importantly you can <strong>connect with others!</strong></p>
+
+<p><strong>Q: How do I connect with others? Walk up to them!<br>A:</strong> Every player has a circle around them, when you come into the circle a Jitsi call will start. The hallway calls are <strong>limited to 4 people</strong>.</p>
+
+<p><strong>Q: Is there a participant limit for the call?<br>A:</strong> Yes, in hallways, regular rooms and outside you can only talk to upto 4 people. <strong>Session rooms have a bigger capacity</strong>, they are primarily dedicated to discussions with speakers after they finish their talk.</p>
+
+<p><strong>Q: How can I connect with a speaker on the map?<br>A:</strong> If the speaker chooses to, they can come into <strong>Session rooms</strong> (classrooms with yellow chairs) and participate in a discussion. When the Jitsi starts, the page will split in half, WorkAdventure on the left side and the call on the right side.</p>
+
+<p><strong>Q: I want to view extra content on the map (schedule/info/photos), how can I do it?<br>A:</strong> Generally, you should walk close enough to the icons representing extra content (schedule / info <img src="/assets/images/workadventure/info.png"> / photos <img src="/assets/images/workadventure/photo.png">). When the link loads, the page will split in half, WorkAdventure on the left side and the content on the right side.</p>
+
+<p><strong>Q: How can I access Fedora DevConf.CZ 2022 badge or Red Hat page?<br>A:</strong> Find the Fedora/DevConf space on the map badge on the map and <strong>claim your badge via displayed URL.</strong></p>
+  </div>
+</header>
+@endsection
