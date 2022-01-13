e62dc33cd682ef40d7cc1a61e8f8ca643a6dd24b
Josef Řídký <jridky@redhat.com>
2022-01-13 13:56:08 +0100

    Add Fedora to supporters in CZ

--- source/_layouts/footercz.blade.php
+++ source/_layouts/footercz.blade.php
@@ -1,4 +1,5 @@
 @section('footer')
 <a href="https://redhat.com" target="_blank"><img class="support-logo pr-3 py-3" src="/assets/images/support/logo-redhat.svg"></a>
+<a href="https://getfedora.org" target="_blank"><img class="support-logo pr-3 py-3" src="/assets/images/support/logo-fedora.svg"></a>
 @endsection
 				
