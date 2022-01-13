e95c62febff70cde745014360ea5e8e5442b3ce1
Josef Řídký <jridky@redhat.com>
2022-01-13 12:56:55 +0100

    Workadventure pages

---
+++ source/cz/workadventure/fedora.blade.php
@@ -0,0 +1,19 @@
+@extends('_layouts.noads')
+
+@section('title')
+<title>Claim your DevConf.CZ 2022 Fedora attendee badge!</title>
+@endsection
+
+@section('body')
+<!-- Full Page Image Header with Vertically Centered Content -->
+<header class="h-100">
+  <div class="container h-100">
+    <div class="row h-100 align-items-center">
+      <div class="col-12 text-center e404">
+	<img src="/assets/images/workadventure/fedora.svg" alt="badge image" class="w-50 pb-5">
+        <h1><a href="https://badges.fedoraproject.org/invitations/f469fe616fe8ec27e286cd8f9b61133b/claim" target="_blank">Claim your attendee badge!</a></h1>
+      </div>
+    </div>
+  </div>
+</header>
+@endsection
---
+++ source/cz/workadventure/redhat.blade.php
@@ -0,0 +1,20 @@
+@extends('_layouts.noads')
+
+@section('title')
+<title>Opportunities are open!</title>
+@endsection
+
+@section('body')
+<!-- Full Page Image Header with Vertically Centered Content -->
+<header class="h-100">
+  <div class="container h-100">
+    <div class="row h-100 align-items-center">
+      <div class="col-12 text-center e404">
+	<img src="/assets/images/workadventure/redhat.png" alt="badge image" class="w-25 pb-5">
+        <h1>Opportunities are open!</h1>
+        <h1>Check out at <a href="https://www.redhat.com/en/jobs" target="_blank">jobs.redhat.com</a></h1>
+      </div>
+    </div>
+  </div>
+</header>
+@endsection
