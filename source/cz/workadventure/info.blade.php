7fd75c11e5f8399a7988c7e56ccb89e4a72fa57a
Josef Řídký <jridky@redhat.com>
2022-01-24 15:34:04 +0100

    Update WA FAQ

--- source/cz/workadventure/info.blade.php
+++ source/cz/workadventure/info.blade.php
@@ -11,17 +11,25 @@
     <h1>WorkAdventure FAQ</h1>
 <p>Hello and welcome to DevConf WorkAdventure map!</p>
 
+<p>The platform is made to recreate the social experience of DevConf.CZ in a game-like environment. You can walk around and explore the place. You can meet people in the hallways and have a chat or join them by the tables in the library or the coffee rooms.</p>
+
+<p>You can now enjoy the after-talk discussions, chatting and meeting new people in a new engaging way!</p>
+
 <p><strong>Q: How does this work?<br>A:</strong> You can move around pressing keys: W/A/S/D (or arrow keys), you can explore the map and most importantly you can <strong>connect with others!</strong></p>
 
 <p><strong>Q: How do I connect with others? Walk up to them!<br>A:</strong> Every player has a circle around them, when you come into the circle a Jitsi call will start. The hallway calls are <strong>limited to 4 people</strong>.</p>
 
 <p><strong>Q: Is there a participant limit for the call?<br>A:</strong> Yes, in hallways, regular rooms and outside you can only talk to upto 4 people. <strong>Session rooms have a bigger capacity</strong>, they are primarily dedicated to discussions with speakers after they finish their talk.</p>
 
-<p><strong>Q: How can I connect with a speaker on the map?<br>A:</strong> If the speaker chooses to, they can come into <strong>Session rooms</strong> (classrooms with yellow chairs) and participate in a discussion. When the Jitsi starts, the page will split in half, WorkAdventure on the left side and the call on the right side.</p>
+<p><strong>Q: How can I connect with a speaker on the map?<br>A:</strong> If the speaker chooses to, they can come into <strong>Session rooms</strong> (classrooms with yellow chairs) and participate in a discussion. The Session rooms number is the same as in <a href="https://hopin.com/events/devconf-cz-2022" target="_blank">Hopin</a> / <a href="http://devconfcz2022.sched.com" target="_blank">sched.com</a>. When the Jitsi starts, the page will split in half, WorkAdventure on the left side and the call on the right side.</p>
 
 <p><strong>Q: I want to view extra content on the map (schedule/info/photos), how can I do it?<br>A:</strong> Generally, you should walk close enough to the icons representing extra content (schedule / info <img src="/assets/images/workadventure/info.png"> / photos <img src="/assets/images/workadventure/photo.png">). When the link loads, the page will split in half, WorkAdventure on the left side and the content on the right side.</p>
 
 <p><strong>Q: How can I access Fedora DevConf.CZ 2022 badge or Red Hat page?<br>A:</strong> Find the Fedora/DevConf space on the map badge on the map and <strong>claim your badge via displayed URL.</strong></p>
+
+<p><strong>Q: How can I manage settings?</strong> <strong>A: In the bottom right corner you have a small camera and microphone</strong>, use that to manage your audio/video while being on the map. <strong>In the top left corner you have a mug and a mail</strong>, the mug can be used to edit your profile, character, name and more! More about the mail icon below.</p>
+
+<p><strong>Q: How can I chat with others?</strong> <strong>A: In the top left corner you have a mug and a mail</strong>, the mail can be used to chat (instead of call) with someone in your close proximity. Although, it doesn’t work in Jitsi calls. The last interactive feature are emotes, when you click on your character you can post an emote for everyone to see. More about the mug icon above. </p>
   </div>
 </header>
 @endsection
---
+++ source/cz/workadventure/photos.blade.php
@@ -0,0 +1,14 @@
+@extends('_layouts.noads')
+
+@section('title')
+<title>Opportunities are open!</title>
+@endsection
+
+@section('body')
+<!-- Full Page Image Header with Vertically Centered Content -->
+<header class="h-100">
+  <div class="container h-100">
+    <h1>Et hic sunt photos</h1>
+  </div>
+</header>
+@endsection
