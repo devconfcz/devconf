51af896b141db21f4f4d3e83e6e44104c5494d09
Josef Řídký <jridky@redhat.com>
2023-02-22 12:36:38 +0100

    add fosstodon reference

--- source/cz/index.blade.php
+++ source/cz/index.blade.php
@@ -483,6 +483,7 @@ We couldn't meet in Brno last year so we worked with our <a href="https://www.do
 
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2256.0277944515196!2d16.598010589157468!3d49.209899867172716!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4712946af848abab%3A0xcbdad60137e0b956!2sFaculty%20of%20Informatics%20Masaryk%20University!5e0!3m2!1sen!2scz!4v1652788388079!5m2!1sen!2scz" loading="lazy" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
 </section>-->
+<a rel="me" href="https://fosstodon.org/@devconf_cz"></a>
 @endsection
 
 @include('_layouts.footercz')
