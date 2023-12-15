6e5c515e946b84430f8fc525deb15f500a739c64
Josef Řídký <jridky@redhat.com>
2023-12-15 15:21:27 +0100

    Update LinkedIn

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -28,7 +28,7 @@
     @endif
 
     | <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-    <script src="https://platform.linkedin.com/in.js" type="text/javascript">lang: en_US</script><script type="IN/Share" data-url="https://www.linkedin.com"></script>
+    <script src="https://platform.linkedin.com/in.js" type="text/javascript">lang: en_US</script><script type="IN/Share" data-url=""></script>
 
     </p>
 
