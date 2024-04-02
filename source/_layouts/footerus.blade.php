480e040ac343a02213370a022bb60951463f53dc
Urvashi Mohnani <umohnani@redhat.com>
2024-04-02 08:35:15 -0400

    Add media sponsors to US website
    Signed-off-by: Urvashi Mohnani <umohnani@redhat.com>

--- source/_layouts/footerus.blade.php
+++ source/_layouts/footerus.blade.php
@@ -2,5 +2,8 @@
 <a href="https://redhat.com" target="_blank"><img class="support-logo pr-3 py-3" src="/assets/images/support/logo-redhat.svg"></a>
 <a href="https://www.openshift.com" target="_blank"><img class="support-logo pr-3 py-3" src="/assets/images/support/logo-openshift.svg"></a>
 <a href="https://getfedora.org" target="_blank"><img class="support-logo pr-3 py-3" src="/assets/images/support/logo-fedora.svg"></a>
+<div class="h5 mont-600">Media Sponsors</div>
+<a href="https://kube.events" target="_blank"><img class="support-logo pr-3 py-3" src="/assets/images/support/logo-kube-events.svg"></a>
+<a href="https://kube.careers" target="_blank"><img class="support-logo pr-3 py-3" src="/assets/images/support/logo-kube-careers.svg"></a>
 @endsection
 				
