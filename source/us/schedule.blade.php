2d6f8af74045c5fe25c5fd0cb2b3a605a605d751
Urvashi Mohnani <umohnani@redhat.com>
2024-05-31 11:44:08 -0400

    Add schedule and registration for US
    Signed-off-by: Urvashi Mohnani <umohnani@redhat.com>

---
+++ source/us/schedule.blade.php
@@ -0,0 +1,90 @@
+@extends('_layouts.page')
+
+@section('social')
+<meta property="og:title" content="DevConf.US | Boston, USA">
+<meta property="og:site_name" content="DevConf.US">
+<meta property="og:description" content="DevConf are free and open, annual, open-source community conferences for community contibutors, sponsored by Red Hat.">
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://devconf.info">
+<meta property="og:image" content="https://devconf.info/assets/images/devconf-us-social.png">
+<meta property="twitter:image" content="https://devconf.info/assets/images/devconf-us-social.png">
+@endsection
+
+@section('css')
+<link rel="stylesheet" href="{{ mix('css/subsite.css', 'assets/build') }}">
+@endsection
+
+@section('title')
+<title>Getting around Boston - DevConf.US</title>
+@endsection
+
+
+@section('body')
+<header class="masthead">
+	<nav class="mb-1 navbar navbar-expand-lg navbar-dark fixed-top masthead">
+		<span class="nav-brand"><a href="/us"><img src="/assets/images/devconf-logo-us-reverse.svg"></a></span>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMobile"
+		    aria-controls="navbarMobile" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarMobile">
+			<ul class="navbar-nav ml-auto pr-5">
+				<li class="nav-item active px-3">
+					<a href="/us/" class="nav-link">Home</a>
+				</li>
+				<li class="nav-item active underline px-3">
+					<a href="/us/aroundboston" class="nav-link">Getting around Boston</a>
+				</li>
+				<li class="nav-item active px-3">
+					<a href="/us/toboston" class="nav-link">Getting to Boston</a>
+				</li>
+				<li class="nav-item active px-3">
+					<a href="/us/hotels" class="nav-link">Hotels</a>
+				</li>
+				<li class="nav-item active px-3">
+					<a href="/us/faq" class="nav-link">FAQ</a>
+				</li>
+				<li class="nav-item active px-3">
+					<a href="/coc" target="_blank" class="nav-link">Policies</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+</header>
+<section class="mx-auto mt-5 pt-5 pb-3 col-lg-11">
+	<div class="row">
+		<div class="h1 mx-5 mont-700 underline position-relative">Schedule</div>
+	</div>
+</section>
+<hr>
+
+<section class="mx-auto mb-5">
+
+		<script type="text/javascript" src="https://pretalx.com/devconf-us-2024/widgets/schedule.js"></script>
+        <pretalx-schedule event-url="https://pretalx.com/devconf-us-2024/" locale="en" format="grid" style="--pretalx-clr-primary: #8E83E4"></pretalx-schedule>
+		<noscript>
+		<div class="pretalx-widget">
+			<div class="pretalx-widget-info-message">
+				JavaScript is disabled in your browser. To access our schedule without JavaScript,
+				please <a target="_blank" href="https://pretalx.com/devconf-us-2024/schedule/">click here</a>.
+			</div>
+		</div>
+		</noscript>
+
+</section>
+
+<script>
+
+    $(document).ready(function() {
+        // matchHeight the contents of each .card-pf and then the .card-pf itself
+        $(".row > [class*='col'] > .card .card-title").matchHeight({property: 'min-height'});
+        $(".row > [class*='col'] > .card > .card-body").matchHeight({property: 'min-height'});
+        $(".row > [class*='col'] > .card > .card-footer").matchHeight({property: 'min-height'});
+        $(".row > [class*='col'] > .card").matchHeight({property: 'min-height'});
+
+    });
+</script>
+
+@endsection
+
+@include('_layouts.footercz')
