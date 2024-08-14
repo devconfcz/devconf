981b6f18d7b0da5680f7178b2facc37f096f126a
Josef Ridky <jridky@redhat.com>
2024-08-14 15:08:11 -0400

    Update IN site
    Signed-off-by: Josef Ridky <jridky@redhat.com>

--- source/in/index.blade.php
+++ source/in/index.blade.php
@@ -16,7 +16,7 @@
 @endsection
 
 @section('title')
-<title>DevConf.IN 2020</title>
+<title>DevConf.IN 2025</title>
 @endsection
 
 
@@ -33,10 +33,10 @@
 				<li class="nav-item active px-3">
 					<a href="#about" class="nav-link local">About & Topics</a>
 				</li>
-				<li class="nav-item active px-3">
+<!--				<li class="nav-item active px-3">
 					<a href="#news" class="nav-link local">News</a>
 				</li>
-				<li class="nav-item active px-3">
+			<li class="nav-item active px-3">
 					<a href="/in/faq" class="nav-link local">FAQ</a>
 				</li>
 <!--				<li class="nav-item active px-3">
@@ -52,9 +52,7 @@
 	<div class="row mr-0 mt-5" style="height: 85%">
 		<div class="col-lg-1 border-right text-center pr-0">
 			<a href="mailto:info@devconf.in" target="_blank" title="Mail"><img src="/assets/images/mail.svg" alt="mail" class="social-icon"></a>
-			<a href="https://www.facebook.com/DevConf.IN" target="_blank" title="Facebook"><img src="/assets/images/facebook.svg" alt="facebook" class="social-icon"></a>
 			<a href="https://twitter.com/devconf_in" target="_blank" title="Twitter"><img src="/assets/images/twitter.svg" alt="twitter" class="social-icon"></a>
-			<a href="https://t.me/devconfin" target="_blank" title="Telegram"><img src="/assets/images/telegram.svg" alt="telegram" class="social-icon"></a>
 			<a href="https://www.youtube.com/channel/UCmYAQDZIQGm_kPvemBc_qwg" target="_blank" title="YouTube"><img src="/assets/images/youtube.svg" alt="youtube" class="social-icon"></a>
 			<a href="https://github.com/devconfcz/devconf" target="_blank" title="Git Hub"><img src="/assets/images/github.svg" alt="github" class="social-icon"></a>
 		</div>
@@ -66,18 +64,15 @@
 			<div class="heading-text mont-400 h5 pb-5">
 				open source community conference
 			</div>
-			  <div class="virtual-stamp mont-700">
-                virtual event
-            </div>
 			<div class="heading-text mont-700 h1 pt-5">
-				December 17-19, 2020
+				February 28 - March 1, 2025
 			</div>
 			<div class="heading-text mont-700 h4">
-				<i class="fa fa-map-marker"></i> All over the world
+				<i class="fa fa-map-marker"></i> MIT World Peace University (MIT WPU), Pune
 			</div>
 			<div class="heading-text pt-5 mt-5">
-				<!-- <a href="https://cfp.devconf.info" target="_blank" class="border p-3 h2 mont-700" style="border-width: 3px !important;">SUBMIT YOUR PROPOSAL</a> -->
-				<a target="_blank" class="border p-3 h2 mont-700" style="border-width: 3px !important;" href="https://hopin.com/events/devconf-in-2021-5cd443de-4683-46f3-9260-2c7e6cb6f618">Register here</a>
+				<!-- <a href="https://cfp.devconf.info" target="_blank" class="border p-3 h2 mont-700" style="border-width: 3px !important;">SUBMIT YOUR PROPOSAL</a> 
+				<a target="_blank" class="border p-3 h2 mont-700" style="border-width: 3px !important;" href="https://hopin.com/events/devconf-in-2021-5cd443de-4683-46f3-9260-2c7e6cb6f618">Register here</a>-->
 			</div>
 		</div>
 	</div>
@@ -91,19 +86,18 @@
 		<div class="col-lg-6 px-5">
 			<h1 class="mont-700 underline pb-4">About DevConf</h1>
 			<div class="os-400">
-                <p>DevConf.IN 2020 is the annual developer's conference organized by Red Hat, India. The conference provides a platform to the FOSS community participants and 
+                <p>DevConf.IN 2025 is the annual developer's conference organized by Red Hat, India. The conference provides a platform to the FOSS community participants and 
 		enthusiasts to come together and engage in knowledge sharing activities through technical talks, workshops, panel discussions, hackathons and much more.</p>
 		
-		<p>While we are disappointed that we won’t be able to see you all in person, we're shifting to an awesome virtual event!</p>
 		<p>There is no admission or ticket charge for DevConf.IN events. However, you are required to complete a free registration.
-		Talks, presentations and workshops will all be in English. Registration is now <a target="_blank" href="https://hopin.com/events/devconf-in-2021-5cd443de-4683-46f3-9260-2c7e6cb6f618">live!</a> </p>
+		Talks, presentations and workshops will all be in English. More information coming soon!</p>
 
                 <p>We are committed to fostering an open and welcoming environment at our conference.
                    We set expectations for inclusive behavior through our code of conduct and media policies, and are prepared to enforce these.</p>
 			</div>
 		</div>
 	</div>
-
+<!--
 	<div class="row">
 		<div class="h1 mx-5 mont-700">Topics</div>
 	</div>
@@ -152,7 +146,7 @@
 		<div class="h1 mx-5 mont-700">Important dates</div>
 	</div>
 	<div class="row">
-		<!-- Keep in pairs -->
+		<!-- Keep in pairs - ->
 		<div class="col-10 col-md-5 mr-md-0 mx-auto py-3 border-bottom">
 			<div class="row">
 				<div class="col-4 h5 mont-700">Oct 16, 2020</div>
@@ -162,7 +156,7 @@
 					</div>
  					<!-- <div class="row">
 						<div class="col os-400"><a href="https://cfp.devconf.info">Submit your proposal &rarr;</a></div>
-					</div> -->
+					</div> - ->
 			</div>
 			</div>
 		</div>
@@ -177,7 +171,7 @@
 			</div>
                 </div>
 
-		<!-- Second pair -->
+		<!-- Second pair - ->
 		<div class="col-10 col-md-5 mr-md-0 mx-auto py-3 border-bottom">
 			<div class="row">
 				<div class="col-4 h5 mont-700">Nov 16 - Dec 8, 2020</div>
@@ -198,7 +192,7 @@
 				</div>
 			</div>
 		</div>
-		<!-- Third pair -->
+		<!-- Third pair - ->
 		<div class="col-10 col-md-5 mr-md-0 mx-auto py-3 border-bottom">
 			<div class="row">
 				<div class="col-4 h5 mont-700">Dec 10, 2020</div>
@@ -220,14 +214,13 @@
 			</div>
 		</div>
 		
-    </div>
+    </div>-->
 </section>
-<!--
-<section id="schedule" class="mx-auto col-lg-12 background-gray text-center mt-5 py-5">
-	<div class="h1 mont-700 py-4 mt-3">Schedule</div>
-	<a class="p-3 mont-700 mb-3 d-inline-block purple-href">SCHEDULE ISN'T AVAILABLE YET</a>
+
+<section class="col-12 px-0 mt-5" style="height: 450px;" id="map">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3783.2464710766876!2d73.8125362758595!3d18.517760969258845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bfb9e53a05f9%3A0x2be5e8da02be693e!2sMIT%20World%20Peace%20University%20(MIT-WPU)!5e0!3m2!1sen!2sus!4v1723662301623!5m2!1sen!2sus" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
 </section>
--->
+
 @endsection
 
 @include('_layouts.footerin')
