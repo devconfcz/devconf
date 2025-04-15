b4200ef53944f5fd3ebe55eede775d58e64c2dfe
Josef Ridky <jridky@redhat.com>
2025-04-15 09:12:15 +0200

    Open registration for CZ
    Signed-off-by: Josef Ridky <jridky@redhat.com>

--- source/cz/hotels.blade.php
+++ source/cz/hotels.blade.php
@@ -53,58 +53,61 @@
 </header>
 <section class="mx-auto mt-5 pt-5 pb-3 col-lg-11">
 	<div class="row">
-		<div class="h1 mx-5 mont-700 underline position-relative">Hotels</div>
-	</div>
-	<div class="row local-nav">
-		<div class="mx-5  mt-4 os-400">
-			<a href="#speakers">For speakers</a>
-			<a href="#attendees">For attendees</a>
-		</div>
-	</div>
-</section>
-<hr>
-<section id="speakers" class="mx-auto mt-5 pb-3 col-lg-11">
-	<div class="row">
-		<div class="h2 mx-5 mont-700 pb-4">For speakers</div>
-
-		<div class="os-400 mx-5">
-		<!--	<p>If you do not require sponsorship to cover your accommodation, please fill out this <a href="https://fedoraproject.limequery.com/156983?lang=en" target="_blank">form</a> to book from our room block.</p>
-
-            <p>By filling out the form, we will auto-generate a reservation email directly to the hotel. If you wish to stay for more than three nights, or if you require a room upgrade beyond the scope of what we offer, please communicate directly with the hotel.</p>
-
-            <p>We have closed the option to request speaker sponsorships for accommodation. If you require any support, please email us at <a href="mailto:speakers@devconf.cz">speakers@devconf.cz</a>.</p>
-            
-            <p><strong>Please make your reservation as soon as possible, as space is limited.</strong></p>
-
-            <p>Accepted DevConf.cz 2024 <strong>sponsored speakers</strong> are eligible to be covered for (up to) three nights of June 12, 13, 14, 15 (16) in a single room at one of our partner hotels listed below. Other premium rooms, all additional nights, “extra person” fees and all other expenses not otherwise mentioned <strong>must be covered separately (e.g. by credit card or cash) in all cases at check-out.</strong></p>
-
-            <p>If you have any questions or concerns, please don't hesitate to contact at <a href="mailto:speakers@devconf.cz">speakers@devconf.cz</a>.</p>-->
-
-            <p>As an accepted speaker, you will receive instructions on how to book from our room blocks as part of your acceptance letter between April 7–13, 2025. Please note that DevConf.CZ does not cover travel or accommodation expenses for speakers.</p>
-		</div>
+		<div class="h1 mx-5 mont-700 underline position-relative">Hotels for attendees</div>
 	</div>
 </section>
 <hr>
 <section id="attendees" class="mx-auto mt-5 pb-3 col-lg-11">
 	<div class="row">
-		<div class="h2 mx-5 mont-700 pb-4">For attendees</div>
 		<div class="os-400 mx-5">
-			<p>We plan to publish the list of partner hotels, along with booking instructions for our room blocks, in mid-April.</p>
+			<p>We are securing a number of discounted rooms for both attendees and speakers. All participants are responsible for booking and covering their own accommodation at one of the partner hotels listed below.</p>
+			<p><strong>Instructions to reserve a hotel room:</strong>
+			<ol><li>Select a hotel from the list below and contact them directly using the provided email address.</li>
+			<li>Use the discount code <strong>DEVCONF-25A in the subject line.</strong></li>
+			<li>Include your name, arrival and departure dates, and any other requirements in your email.</li></ol>
+			<div class="accordion w-100 pb-3" id="template">
+    			<div class="card">
+	    			<div class="card-header collapsed mont-600" id="template-heading" data-toggle="collapse" 
+				     data-target="#collapse-template" aria-expanded="true" aria-controls="collapse-template">
+						Email template for hotel room booking (replace &lt; ... &gt; parts with actual information)
+						<i class="fa"></i>
+					
+				    </div>
+
+				    <div id="collapse-template" class="collapse" aria-labelledby="template-heading" data-parent="#template">
+					    <div class="card-body os-400">
+						    <strong>Subject:</strong> DEVCONF-25A Hotel room booking for &lt;NAME&gt;<br><br>
+						    <strong>Text:</strong><br>
+						    Dear &lt;HOTEL NAME&gt;<br><br>
+                            I would like to reserve a &lt;ROOM TYPE&gt; for &lt;X&gt; nights from &lt;CHECK-IN DATE&gt; to &lt;CHECK-OUT DATE&gt;, for &lt;GUEST NAME&gt;.<br><br>
+                            Additional notes regarding the booking: &lt;None / Notes from the guest&gt;<br><br>
+                            Could you please confirm the booking with &lt;me / GUEST NAME&gt; directly?<br><br>
+                            Many thanks,<br>
+                            &lt;YOUR NAME&gt;
+					    </div>
+				    </div>
+			    </div>
+    		</div>
+			
+			<p><strong>Bookings through third-party sites (e.g. booking.com), even if they are for one of our partner hotels, will not have the same price.</strong></p>
+            <p>All prices are <strong>per one night!</strong> The final price for accommodation depends on the partner hotel (accommodation taxes are usually charged on the spot).
+            Contact the partner hotel directly to save your spot and for additional information.</p>
 		</div>
 	</div>
 </section>
-<!--<hr>
+<hr>
 <section class="mx-auto col-lg-11">
-	<div class="row">
+   <div class="row">
+	    
 		<div class="col-lg-4">
 			<div class="h2 pt-3 pb-4 mx-5 mont-700">Avanti</div>
 
 			<div class="os-400 mx-5">
 				<p><a href="https://www.hotelavanti.cz/en/" target="_blank">https://www.hotelavanti.cz/en/</a></p>
-				<!-- <p class="alert alert-warning d-inline-block">Less than 10 classic rooms available</p> ->
-				<p>Single Standard room - 1.850 CZK/night<br>
-				Double Standard room - 2.100 CZK/night<br>
-				<p>The rate is including breakfast.</p>
+				<p>Single Standard room - 1.950 CZK/night<br>
+				Double Standard room - 2.200 CZK/night</p>
+				<p>The rate includes breakfast.</p>
+				<p>Email for making reservations is:<br><a href="mailto:hotel@hotelavanti.cz">hotel@hotelavanti.cz</a></p>
 			</div>
 		</div>
 		<div class="col-lg-4">
@@ -112,10 +115,10 @@
 
 			<div class="os-400 mx-5">
 				<p><a href="https://www.vista-hotel.cz/" target="_blank">https://www.vista-hotel.cz/</a></p>
-				<!-- <p class="alert alert-danger d-inline-block">No rooms available</p> ->
-				<p>Single Standard room 1.650 CZK/night<br>
-				Double Standard room 1.900 CZK/night</p>
-				<p>The room rate includes buffet breakfast, Wi-Fi and parking.</p>
+				<p>Single Standard room 1.700 CZK/night<br>
+				Double Standard room 2.000 CZK/night</p>
+				<p>The rate includes buffet breakfast, Wi-Fi and parking.</p>
+				<p>Email for making reservations is:<br><a href="mailto:hotel@vista-hotel.cz">hotel@vista-hotel.cz</a></p>
 			</div>
 		</div>
 		<div class="col-lg-4">
@@ -123,14 +126,12 @@
 
 			<div class="os-400 mx-5">
 				<p><a href="https://www.continentalbrno.cz/en/" target="_blank">https://www.continentalbrno.cz/en/</a></p>
-				<!-- <p class="alert alert-success d-inline-block">10+ classic rooms available</p> ->
-				<p>Single Standard room - 1.700 CZK/night<br>
-				Single Superior room - 2.000 CZK/night<br>
-				Double Standard room - 2.150 CZK/night<br>
-				Double Standard room (used as single room) - 1.850 CZK/night<br>
-				Double Superior room - 2.350 CZK/night<br>
-				Double Superior room (used as single room) - 2.150 CZK/night</p>
-				<p>The rate is including breakfast.</p>
+				<p>Single Standard room - 1.900 CZK/night<br>
+				Single Superior room - 2.300 CZK/night<br>
+				Double Standard room - 2.400 CZK/night<br>
+				Double Superior room - 2.650 CZK/night</p>
+				<p>The rate includes breakfast and all taxes.</p>
+				<p>Email for making reservations is:<br><a href="mailto:info@continentalbrno.cz">info@continentalbrno.cz</a></p>
 			</div>
 		</div>
 		<div class="col-lg-4">
@@ -138,9 +139,9 @@
 
 			<div class="os-400 mx-5">
 				<p><a href="https://grandhotelbrno.cz/en/" target="_blank">https://grandhotelbrno.cz/en/</a></p>
-				<!-- <p class="alert alert-danger d-inline-block">No rooms available</p> ->
-				<p>Single Standard room 2.150 CZK/night</p>
-				<p>The rate is including breakfast.</p>
+				<p>Single Standard room 2.250 CZK/night</p>
+				<p>The rate includes breakfast.</p>
+				<p>Email for making reservations is:<br><a href="mailto:reservation@grandhotelbrno.cz">reservation@grandhotelbrno.cz</a></p>
 			</div>
 		</div>
 		<div class="col-lg-4">
@@ -148,14 +149,14 @@
 
 			<div class="os-400 mx-5">
 				<p><a href="http://hotelpassage.eu/" target="_blank">http://hotelpassage.eu/</a></p>
-				<!-- <p class="alert alert-success d-inline-block">10+ classic rooms available</p> ->
-				<p>Single Standard room 3.100 CZK/night<br>
-				Double Classic room 3.430 CZK/night</p>
-				<p>The rate is including breakfast and Wi-Fi.</p>
+				<p>Single Standard room 3.000 CZK/night<br>
+				Double Standard room 3.300 CZK/night</p>
+				<p>The rate includes breakfast and Wi-Fi.</p>
+				<p>Email for making reservations is:<br><a href="mailto:rezervace@hotelpassage.eu">rezervace@hotelpassage.eu</a></p>
 			</div>
 		</div>
 	</div>
-</section>-->
+</section>
 @endsection
 
 @include('_layouts.footercz')
