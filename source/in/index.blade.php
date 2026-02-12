ced6d3a6d49037b7e249e437cbd7596684425cba
Josef Ridky <jridky@redhat.com>
2026-02-12 13:58:45 +0100

    Reg widget update
    Signed-off-by: Josef Ridky <jridky@redhat.com>

--- source/in/index.blade.php
+++ source/in/index.blade.php
@@ -118,11 +118,11 @@
     <div class="mt-4 py-5">
     	<h1 class="mont-700 underline pb-4">Registration</h1>
         <div class="os-400">
-            Regular tickets are sold out. You can still join the waitlist. If a spot opens, you’ll have 24 hours to redeem your invite via email.<br><br>
+            Regular tickets are sold out. If a spot opens up while you are on the waitlist, you’ll have 24 hours to redeem your invite via email.<br><br>
 
 A valid ticket is required for entry. If you don't have one, please note that we may release at-the-door tickets on the day of the event depending on venue capacity. We will announce availability on social media. Please do not arrive before 10:30 AM to allow us time to assess attendance.
         </div>
-        <link rel="stylesheet" type="text/css" href="https://pretix.eu/devconf/devconfin2026/widget/v2.css">
+        <!--<link rel="stylesheet" type="text/css" href="https://pretix.eu/devconf/devconfin2026/widget/v2.css">
         <script type="text/javascript" src="https://pretix.eu/widget/v2.en.js" async></script>
 
         <div class="pretix-widget-compat" event="https://pretix.eu/devconf/devconfin2026/" single-item-select="button"></div>
@@ -132,7 +132,7 @@ A valid ticket is required for entry. If you don't have one, please note that we
                     JavaScript is disabled in your browser. To access our ticket shop without JavaScript, please <a target="_blank" rel="noopener" href="https://pretix.eu/devconf/devconfin2026/">click here</a>.
                 </div>
            </div>
-        </noscript>
+        </noscript> -->
     </div>
 </section>
 <section id="topics" class="mx-auto col-lg-11">	
